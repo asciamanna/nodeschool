a9c2fafd182d58b8b65224d7946e0ba9359572bb
Anthony Sciamanna <asciamanna@gmail.com>
2015-01-24 06:07:11 -0500

    solution to functional-javascript filter exercise.

---
+++ functional-javascript/filter.js
@@ -0,0 +1,9 @@
+function getShortMessages(messages) {
+	return messages.map(function (item) {
+		return item.message;
+	}).
+	filter(function (item) {
+		return item.length < 50;
+	});
+}
+module.exports = getShortMessages;
