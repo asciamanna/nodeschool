a7d844012d6dd77c032a40717e39e7e22ccd1ee4
Anthony Sciamanna <asciamanna@gmail.com>
2015-01-29 12:15:20 -0500

    solution to expressworks param pam pam exercise.

---
+++ expressworks/param-pam-pam.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var app = express();
+app.put('/message/:id', function(req, res) {
+	res.end(require('crypto').createHash('sha1').update(new Date().toDateString() + req.params.id).
+		digest('hex'));
+});
+app.listen(process.argv[2]);
