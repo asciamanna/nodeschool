358e815afcddf56f9dc73751be52202778b30954
Anthony Sciamanna <asciamanna@gmail.com>
2015-01-31 06:44:50 -0500

    reformatting sort me solution

--- lololodash/sort-me.js
+++ lololodash/sort-me.js
@@ -3,7 +3,9 @@
 var _ = require('lodash');
 
 var sort = function(items) {
-	return _.sortBy(items, function(item) { return -item.quantity; });
+	return _.sortBy(items, function(item) { 
+		return -item.quantity; 
+	});
 };
 
 module.exports = sort;
