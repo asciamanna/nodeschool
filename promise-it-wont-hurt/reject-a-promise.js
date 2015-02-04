6caca54547778daaf3331778201db5177f16ac67
Anthony Sciamanna <asciamanna@gmail.com>
2015-02-04 17:20:16 -0500

    solution to reject a promise exercise.

---
+++ promise-it-wont-hurt/reject-a-promise.js
@@ -0,0 +1,9 @@
+var q = require('q');
+var defer = q.defer();
+
+function printError(error) {
+	console.log(error.message);
+}
+
+defer.promise.then(null, printError);
+setTimeout(defer.reject, 300, new Error('REJECTED!'));
